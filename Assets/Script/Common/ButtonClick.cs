c7c57b3a7e0d3a37024b3060f79bb8fdae289836
maycoco <wangths@163.com>
2018-04-17 11:11:12 +0800

    2018-4-17 增加按钮动效果
    2018-4-17 增加按钮动效果

---
+++ Assets/Script/Common/ButtonClick.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ButtonClick : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		//Button btn = this.GetComponent<Button> ();
+		//btn.OnPointerDown = OnPointerDown;
+		EventTriggerListener.Get(gameObject).onDown = OnPointerDownHandler;
+		EventTriggerListener.Get(gameObject).onClick = OnClickHandler;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+		
+	public void OnPointerDownHandler(GameObject Obj){
+		transform.DOScale (new Vector3(1.02f,1.02f,1.02f), 0.3f);
+	}
+
+	public void OnClickHandler(GameObject Obj){
+		transform.DOScale (new Vector3(1.0f,1.0f,1.0f), 0.1f);
+	}
+}
+
