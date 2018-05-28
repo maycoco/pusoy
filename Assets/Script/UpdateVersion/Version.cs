3dbf2baaeed7a8789ada9a950c2f0dfa39f203d3
lzhig <lzhig@outlook.com>
2018-05-28 22:18:51 +0800

    1.0.1

--- Assets/Script/UpdateVersion/Version.cs
+++ Assets/Script/UpdateVersion/Version.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 
 public static class VersionInfo
 {
-    public static readonly string version = "0.2.2";
-    public static readonly int version_code = 22;
+    public static readonly string version = "1.0.1";
+    public static readonly int version_code = 101;
 }
 
 public class Version : MonoBehaviour {
